661c30e5d5c016a340e2795d69c839cfc6673a17
AkifPinarci <mapinarci1905@gmail.com>
2021-10-17 23:37:12 -0700

    Add: new things

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
+# Useful notes
+# Learn more about Jinja templates
+
+
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def home(name):
-    return render_template('index.html', content=["Mehmet", "Akif", "Pinarci"], r=2)
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return "This is about page"
-
-@app.route('/input')
-def login():
-    return f"Hello"
-
-@app.route('/admin')
+@app.route('/your-url')
 def admin():
     return redirect(url_for("login"))
 
-if __name__ == '__main__':
-    app.run()
-
-
+if __name__ == "__main__":
+    app.run(debug=True)
