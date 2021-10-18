0f1a440ae0d838e31f92333745c9a48927e7b07d
AkifPinarci <mapinarci1905@gmail.com>
2021-10-18 14:20:36 -0700

    Urls shortener added

--- app.py
+++ app.py
@@ -2,17 +2,45 @@
 # Learn more about Jinja templates
 
 
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, flash
+import json
+import os.path
+from werkzeug.utils import secure_filename
+
 
 app = Flask(__name__)
+app.secret_key = 'hg234jhg5f23j4hg52'
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/your-url')
-def admin():
-    return "Mehmet Akif Pinarci"
+@app.route('/your-url', methods = ['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        urls = {}
+        
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
 
+        if request.form['code'] in urls.keys():
+            flash('That short name has already been taken. please select a different name')
+            return redirect(url_for('home'))
+
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url' : request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code'] + secure_filename(f.filename)
+            f.save(r'C:\Users\mapin\OneDrive\Desktop\LearningFlask\Learning-Flask' + full_name)
+            urls[request.form['code']] = {'file' : full_name}
+
+
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+        return render_template('your_url.html', code = request.form['code'])
+    else:
+        return redirect(url_for('home'))
 if __name__ == "__main__":
     app.run(debug=True)
